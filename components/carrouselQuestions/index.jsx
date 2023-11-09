00e6cea1a0fdfd057dc9c1787b2f8b5a52c6c83b
Pilar Baroni <122078706+PilarBaroni@users.noreply.github.com>
2023-11-09 20:58:15 -0300

    NavBar

--- components/carrouselQuestions/index.jsx
+++ components/carrouselQuestions/index.jsx
@@ -4,29 +4,29 @@ import { Accordion, AccordionBody, AccordionHeader, AccordionList } from "@tremo
 
 export default () => (
   
-  <AccordionList className="max-w-3x1 mx-100">
+  <AccordionList className="max-w-3x1 mx-100 mx-100 bg-gradient-to-b from-Az1 via-grey text-white">
     <h2 className="w-full h-20 flex items-center justify-center text-white text-3xl font-bold avant-garde-bold">Preguntas frecuentes:</h2>
     <Accordion>
-      <AccordionHeader className="font-normal avant-garde-regular">¿Qué es una mesa de ayuda y por qué mi empresa necesita una?</AccordionHeader>
-      <AccordionBody className="font-normal avant-garde-regular">
+      <AccordionHeader className="font-normal avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-grey text-white" >¿Qué es una mesa de ayuda y por qué mi empresa necesita una?</AccordionHeader>
+      <AccordionBody className="font-bold avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-Az4 text-white">
         Una mesa de ayuda es un sistema que ayuda a las empresas a gestionar y resolver las solicitudes de servicio de manera eficiente. Es esencial para mejorar la satisfacción del cliente y mantener un registro de problemas y soluciones.
       </AccordionBody>
     </Accordion>
     <Accordion>
-      <AccordionHeader className="font-normal avant-garde-regular">¿Cómo puedo registrarme en la plataforma de gestión de tickets?</AccordionHeader>
-      <AccordionBody className="font-normal avant-garde-regular">
+      <AccordionHeader className="font-normal avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-grey text-white">¿Cómo puedo registrarme en la plataforma de gestión de tickets?</AccordionHeader>
+      <AccordionBody className="font-bold avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-Az4 text-white">
         El proceso de registro es simple. Visita nuestra página de inicio, haz clic en "Registrarse" y sigue las instrucciones para crear una cuenta.
       </AccordionBody>
     </Accordion>
     <Accordion>
-      <AccordionHeader className="font-normal avant-garde-regular">¿Qué beneficios obtendré al utilizar su plataforma?</AccordionHeader>
-      <AccordionBody className="font-normal avant-garde-regular">
+      <AccordionHeader className="font-normal avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-grey text-white">¿Qué beneficios obtendré al utilizar su plataforma?</AccordionHeader>
+      <AccordionBody className="font-bold avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-Az4 text-white">
         Al usar nuestra plataforma, obtendrás una gestión más eficiente de las solicitudes de servicio, informes detallados sobre el rendimiento y la capacidad de mejorar la satisfacción del cliente.
       </AccordionBody>
     </Accordion>
     <Accordion>
-        <AccordionHeader className="font-normal avant-garde-regular">¿Cómo puedo crear y seguir un ticket de servicio?</AccordionHeader>
-      <AccordionBody className="font-normal avant-garde-regular">
+        <AccordionHeader className="font-normal avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-grey text-white">¿Cómo puedo crear y seguir un ticket de servicio?</AccordionHeader>
+      <AccordionBody className="font-bold avant-garde-regular bg-gradient-to-b from-Az4 via-Az4 to-Az4 text-white">
         Después de iniciar sesión, simplemente haz clic en "Crear Ticket" y completa la información requerida. Puedes seguir el estado de tus tickets en tu panel de control.
       </AccordionBody>
     </Accordion>
--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="bg-gradient-to-r from-Az3 via-Az2 to-Az1 p-4 text-white flex flex-row items-center justify-between">
+    <div className="bg-gradient-to-r from-Az3 via-Az2 to-Az1 p-4 text-white flex flex-row items-center justify-between shadow-lg">
       <div>
         <Link href="/home">
           <Image src={logotipo} width={250} height={250} alt="Logotipo" />
         </Link>
       </div>
       <div>
-        <button className="mr-2 font-bold avant-garde-bold">
+        <button className="mr-2 py-1 px-1 font-bold avant-garde-bold text-base rounded bg-Az3 text-white-800 font-bold avant-garde-bold transition duration-300 ease-in-out hover:bg-Az3 hover:text-Az4 hover:shadow-lg">
           <Link href="/login">Login</Link>
         </button>
 
-        <button className="bg-white text-blue-800 font-bold avant-garde-bold">
+        <button className="mr-2 py-1 px-1 font-bold avant-garde-bold text-base rounded bg-Az3 text-white-800 font-bold avant-garde-bold transition duration-300 ease-in-out hover:bg-Az3 hover:text-Az4 hover:shadow-lg">
           <Link href="/register">Register</Link>
         </button>
       </div>
