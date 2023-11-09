b5f975c29eccd5a71f5329c170c986a35bddfc1b
Joel Lopez <djoel95@hotmail.com>
2023-11-08 21:25:02 -0400

    feature/useractivator

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as RegisterForm } from './registerForm/index.jsx';
 export { default as LoginForm } from './loginForm/index.jsx';
 export { default as ForgetPassword } from './forgetPassword/index.jsx';
 export { default as ChangePassword } from './changePassword/index.jsx';
+export { default as UserActivation } from './userActivation/index.jsx';
