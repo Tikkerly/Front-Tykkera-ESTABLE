67c81e51dc885eb6f32ac3ece9942def9257a70b
Sebastian Gonzalez Conosciuto <conosciutosebastian@gmail.com>
2023-11-09 23:59:34 -0300

    hotfix

--- components/loginForm/index.jsx
+++ components/loginForm/index.jsx
@@ -137,10 +137,6 @@ const LoginForm = () => {
             Recordarme
           </label>
         </div>
-
-        <Link href="/register" className="text-sm mt-1">
-          Registrarme
-        </Link>
         <div className="mt-2 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <button type="submit">Iniciar sesión</button>
         </div>
