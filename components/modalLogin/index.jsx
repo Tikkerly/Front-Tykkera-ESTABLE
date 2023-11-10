d0d17fe4385c276e286dcc1ae5bed09305d77de1
Joel Lopez <djoel95@hotmail.com>
2023-11-10 10:08:33 -0400

    feature/calendar

--- components/modalLogin/index.jsx
+++ components/modalLogin/index.jsx
@@ -12,9 +12,10 @@ const ModalLogin = ({ isVisible, onClose }) => {
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-10" id="wrapper" onClick={handleClose}>
       <div className="w-[800] flex flex-col justify-center items-center">
         <div className="bg-243244 p-2 rounded">
+        <button className="text-white text-xl w-8 h-8 absolute top-25 right-15 bg-Az4 bg-opacity-40 rounded-full  shadow-md font-bold avant-garde-bold hover:bg-red-400" onClick={() => onClose()}>X</button>
+    
           <LoginForm />
         </div>
-        <button className="text-white text-xl w-8 h-8  bg-Az4 bg-opacity-40 rounded-full  shadow-md font-bold avant-garde-bold hover:bg-red-400" onClick={() => onClose()}>X</button>
       </div>
     </div>
   );
