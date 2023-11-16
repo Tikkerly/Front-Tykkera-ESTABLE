c317d6a7cdfb414e991b4268fa911267bfc5b7a5
Joel Lopez <djoel95@hotmail.com>
2023-11-16 11:09:10 -0400

    feature/stylosJowell

--- components/SubmitButton/index.jsx
+++ components/SubmitButton/index.jsx
@@ -2,6 +2,6 @@
 
 export default function SubmitButton ({text, type}) {
     return (
-        <button className="avant-garde-bold font-bold bg-Az5 text-gray px-6 py-3 rounded-full transition duration-300 hover:shadow-md "  type={type}>{text}</button>
+        <button className="avant-garde-bold font-bold bg-Az5 text-gray px-6 py-3 w-auto rounded-full transition duration-300 hover:shadow-md "  type={type}>{text}</button>
     )
 }
