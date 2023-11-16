bb27c85c4a96b06070bdd0567cf06904c42815ef
Joel Lopez <djoel95@hotmail.com>
2023-11-16 12:12:41 -0400

    feature/stylosJowell

--- components/description/index.jsx
+++ components/description/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SubmitButton } from "..";
+import styles from "./style.module.css";
 
 
 
@@ -8,9 +9,13 @@ const Description = () => {
     <>
 
 
-    <div className=" bg-gray-100 mb-40 p-40 w-screen h-full rounded-lg shadow-md flex flex-col items-center justify-center bg-image ">
-      <div className="content">
-        <h1 className="title text-7xl font-bold leading-none text-Az3 bg-gradient-to-r from-Az1 to-Az3 bg-clip-text text-transparent">Tykkera</h1>
+<div className={` mb-40 p-40 bg-white opacity-90 w-screen h-full rounded-lg shadow-md flex flex-col items-center justify-center ${styles["bg-image"]}`}>
+      
+      <div className="w-screen h-full ">
+      <div className="w-auto h-full p-20 bg-white bg-opacity-80 rounded-lg "> 
+
+      <div className="content ">
+        <h1 className="title mt-20 text-7xl font-bold leading-none text-Az3 bg-gradient-to-r from-Az1 to-Az3 bg-clip-text text-transparent">Tykkera</h1>
         <div className="aurora">
           <div className="aurora__item"></div>
           <div className="aurora__item bg-blue-500"></div>
@@ -19,16 +24,17 @@ const Description = () => {
         </div>
         <p className="subtitle text-2xl text-Az3">Simplificando lo complejo a un ticket</p>
       </div>
-      <h2 className="leading-loose bg-az1 w-1/2 font-regular text-2xl text-gray flex avant-garde-regular ml-40 mr-40 description">
+      <h2 className="leading-loose bg-az1 w-1/2 font-regular text-2xl text-gray flex avant-garde-regular   description">
         Facilitamos la gestión de tickets de mesa de ayuda para empresas de reparación y 
-        mantenimiento de equipos tecnológicos. Nos especializamos en simplificar el registro, ordenamiento y 
-        seguimiento de tickets, ofreciendo una plataforma intuitiva y eficiente que ayuda a pequeñas y medianas empresas a optimizar su operación.
+        mantenimiento de equipos tecnológicos.
       </h2>
       <SubmitButton text={"Empezar"} className="button"/>
+      </div>
+      </div>
     </div>
    
       </>
   );
 }
 
-export default Description;
+export default Description; 
