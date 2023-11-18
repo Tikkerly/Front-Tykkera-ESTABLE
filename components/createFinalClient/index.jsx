1875ca03ab56497c6bf2592ef70676d8881b23c7
alejonoel <alejoescobar2015@gmail.com>
2023-11-18 10:25:30 -0300

    sin imágenes

--- components/createFinalClient/index.jsx
+++ components/createFinalClient/index.jsx
@@ -180,7 +180,7 @@ const CreateFinalClient = () => {
           type="submit"
           className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          Registrar Técnico
+          Crear Cliente Final
         </button>
       </div>
     </form>
