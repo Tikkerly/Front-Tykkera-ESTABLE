a287abb60008d68ba3fa23f0bac8b0b296060558
alejonoel <alejoescobar2015@gmail.com>
2023-11-24 09:50:57 -0300

    tickets con validaciones y campo empresas

--- utils/validation/index.js
+++ utils/validation/index.js
@@ -119,6 +119,52 @@ const validation = (type, values) => {
         errors.serviceTypes = "El campo es obligatorio";
       }
       return errors;
+    case "ticket":
+      if (!values.serviceType) {
+        errors.serviceType = "El campo es obligatorio";
+      } else if (values.serviceType.length < 5) {
+        errors.serviceType = "Debe tener al menos 5 caracteres";
+      }
+
+      if (!values.serviceDescription) {
+        errors.serviceDescription = "El campo es obligatorio";
+      } else if (values.serviceDescription.length < 10) {
+        errors.serviceDescription = "Debe tener al menos 10 caracteres";
+      }
+
+      if (!values.startDate) {
+        errors.startDate = "El campo es obligatorio";
+      } else if (new Date(values.startDate) < new Date(new Date().setHours(0, 0, 0, 0))) {
+        console.log(new Date(new Date().setHours(0, 0, 0, 0)))
+        console.log(new Date(values.startDate))
+        errors.startDate = "La fecha de inicio no puede ser menor o igual a la fecha actual";
+      }
+
+      if (values.ammount === "" || values.ammount < 1) {
+        errors.ammount = "El valor no puede ser menor a 1";
+      }
+
+      if (values.cost === "" || values.cost < 1) {
+        errors.cost = "El costo no puede ser menor a 1";
+      }
+
+      if (values.others === "" || values.others < 0) {
+        errors.others = "Los otros costos no pueden ser menores a 0";
+      }
+
+      if (!values.finalClient_id) {
+        errors.finalClient_id = "El campo es obligatorio";
+      }
+
+      if (!values.technician_id) {
+        errors.technician_id = "El campo es obligatorio";
+      }
+
+      if (!values.serviceClient_id) {
+        errors.serviceClient_id = "El campo es obligatorio";
+      }
+
+      return errors;
   }
 };
 
