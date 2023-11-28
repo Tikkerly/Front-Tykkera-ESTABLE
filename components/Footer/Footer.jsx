fb6e08f6f274827a57141bd1bd0c0d80a6291cd1
Pilar Baroni <122078706+PilarBaroni@users.noreply.github.com>
2023-11-28 18:13:47 -0300

    footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,49 +1,64 @@
 import Link from "next/link";
 
 const Footer = () => {
+  const handleLinkClick = (url) => {
+    window.open(url, '_blank');
+  };
   return (
-    <div className="w-full bg-Az1 mt-10">
-      <div className="flex justify-between items-center p-8">
-        <h1 className="avant-garde-bold font-bold text-gray-100 mt-2">
+    <div className="w-full bg-Az1 mt-10 bg-opacity-90">
+      <div className="flex justify-between items-center ">
+        <h1 className="avant-garde-regular font-regular text-gray-100 mt-2 p-2 border-t-2 border-b-2 border-dashed  border-Az2" style={{marginLeft: "250px", marginRight: "70px"}}>
           Creado por:
         </h1>
-        <div className="grid grid-cols-3 gap-2 avant-garde-bold font-bold text-gray-100 justify-center">
-          <Link
-            href="https://www.linkedin.com/in/pilar-baroni-3b4650281/"
-            style={{ textDecoration: "none", color: "inherit" }}
+        <div className="grid grid-cols-3 gap-2 avant-garde-bold font-bold text-gray-100 gap-4 mt-4" style={{ justifyItems: 'center' }}>
+        <p
+            className="text-sm hover:text-Az2 cursor-pointer"
+            onClick={() =>
+              handleLinkClick('https://www.linkedin.com/in/pilar-baroni-3b4650281/')
+            }
           >
-            <h1 className="text-sm hover:text-Az2 hover:underline">Baroni, Pilar</h1>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/pilar-baroni-3b4650281/"
-            style={{ textDecoration: "none", color: "inherit" }}
+            Baroni, Pilar
+          </p>
+          <p
+            className="text-sm hover:text-Az2 cursor-pointer"
+            onClick={() =>
+              handleLinkClick('https://www.linkedin.com/in/pilar-baroni-3b4650281/')
+            }
           >
-            <h1 className="text-sm hover:text-Az2 hover:underline">Conoscuito, Sebastian</h1>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/pilar-baroni-3b4650281/"
-            style={{ textDecoration: "none", color: "inherit" }}
+            Conoscuito, Sebastian
+          </p>
+<p
+            className="text-sm hover:text-Az2 cursor-pointer"
+            onClick={() =>
+              handleLinkClick('https://www.linkedin.com/in/pilar-baroni-3b4650281/')
+            }
           >
-            <h1 className="text-sm hover:text-Az2 hover:underline">Lopez, Joel</h1>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/pilar-baroni-3b4650281/"
-            style={{ textDecoration: "none", color: "inherit" }}
+            Lopez, Joel
+          </p>
+          <p
+            className="text-sm hover:text-Az2 cursor-pointer"
+            onClick={() =>
+              handleLinkClick('https://www.linkedin.com/in/pilar-baroni-3b4650281/')
+            }
           >
-            <h1 className="text-sm hover:text-Az2 hover:underline">Tamayo, Hugo</h1>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/pilar-baroni-3b4650281/"
-            style={{ textDecoration: "none", color: "inherit" }}
+            Tamayo, Hugo
+          </p>
+          <p
+            className="text-sm hover:text-Az2 cursor-pointer"
+            onClick={() =>
+              handleLinkClick('https://www.linkedin.com/in/pilar-baroni-3b4650281/')
+            }
           >
-            <h1 className="text-sm hover:text-Az2 hover:underline">Klainbar, Mauricio</h1>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/pilar-baroni-3b4650281/"
-            style={{ textDecoration: "none", color: "inherit" }}
+            Klainbar, Mauricio
+          </p>
+          <p
+            className="text-sm hover:text-Az2  cursor-pointer"
+            onClick={() =>
+              handleLinkClick('https://www.linkedin.com/in/pilar-baroni-3b4650281/')
+            }
           >
-            <h1 className="text-sm hover:text-Az2 hover:underline">Escobar, Alejo</h1>
-          </Link>
+            Escobar, Alejo
+          </p>
         </div>
       </div>
     </div>
