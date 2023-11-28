6acb096e333a01dfcae537d8c659ae1707a47bb3
Pilar Baroni <122078706+PilarBaroni@users.noreply.github.com>
2023-11-28 19:01:52 -0300

    css footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,15 +5,15 @@ const Footer = () => {
   };
   return (
     <div className="w-full bg-Az1 mt-10 bg-opacity-90">
-      <div className="flex justify-between items-center ">
+      <div className="flex justify-between items-center justify-around ">
         <h1
           className="avant-garde-regular font-regular text-gray-100 mt-2 p-2 border-t-2 border-b-2 border-dashed  border-Az2"
-          style={{ marginLeft: "250px", marginRight: "70px" }}
+          
         >
           Sobre nosotros
         </h1>
         <div
-          className="grid grid-cols-3 gap-2 avant-garde-bold font-bold text-gray-100 gap-4 mt-4"
+          className="grid grid-cols-3 gap-2 avant-garde-bold font-bold text-gray-100  mt-4"
           style={{ justifyItems: "center" }}
         >
           <p
