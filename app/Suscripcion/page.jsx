5c11d59212d053ec6bfb25f30dfa6f94b95fa1d2
Hugo Tamayo <doctamayot@hotmail.com>
2023-11-29 13:11:08 -0500

    OK para entrega final2

--- app/Suscripcion/page.jsx
+++ app/Suscripcion/page.jsx
@@ -3,13 +3,15 @@ import { Suscripcion } from "@/components";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const Suscription = () => {
+  const user = useSelector((state) => state.auth.user);
   const router = useRouter();
 
   useEffect(() => {
     const token = Cookies.get("token");
-    if (!token) {
+    if (!token || user.rol === "Admin") {
       router.push("/ingresar");
     }
   }, []);
--- app/user/layout.jsx
+++ app/user/layout.jsx
@@ -1,17 +1,27 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SideBar from "@/components/sideBar";
 import styles from "./styles.module.css";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
 
 const UserLayout = ({ children }) => {
+  const [newUser, setNewUser] = useState({});
+  const user = useSelector((state) => state.auth.user);
   const router = useRouter();
 
   useEffect(() => {
+    setNewUser(user);
     const token = Cookies.get("token");
     if (!token) {
       router.push("/ingresar");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user.rol === "Admin") {
+      router.push("/administrador");
     }
   }, []);
 
